d74e3fec8155e257fd1b12830327c54401ef318d
Harikumaran906 <kumaranh906@gmail.com>
2025-11-21 17:01:15 +0530

    Switch fetch to /api and update Stripe URLs

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -5,58 +5,31 @@ import mongoose from "mongoose";
 export async function GET(request, { params }) {
   await connectDB();
 
-  const id = params.id;
+  console.log("==== DEBUG: PARAMS RECEIVED ====");
+  console.log(params);
 
-  let product;
+  const id = params.id;
+  console.log("==== DEBUG: ID RECEIVED ====");
+  console.log(id);
+
+  let product = null;
 
   if (mongoose.Types.ObjectId.isValid(id)) {
+    console.log("==== DEBUG: Valid ObjectId ====");
     product = await Product.findById(id);
+  } else {
+    console.log("==== DEBUG: Invalid ObjectId ====");
   }
 
   if (!product) {
+    console.log("==== DEBUG: Trying findOne ====");
     product = await Product.findOne({ _id: id });
   }
 
+  console.log("==== DEBUG: RESULT PRODUCT ====");
+  console.log(product);
+
   return new Response(JSON.stringify(product), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
-
-export async function PUT(request, { params }) {
-  await connectDB();
-  const id = params.id;
-  const body = await request.json();
-
-  let updated;
-
-  if (mongoose.Types.ObjectId.isValid(id)) {
-    updated = await Product.findByIdAndUpdate(id, body, { new: true });
-  }
-
-  if (!updated) {
-    updated = await Product.findOneAndUpdate({ _id: id }, body, { new: true });
-  }
-
-  return new Response(JSON.stringify(updated), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
-
-export async function DELETE(request, { params }) {
-  await connectDB();
-  const id = params.id;
-
-  let deleted;
-
-  if (mongoose.Types.ObjectId.isValid(id)) {
-    deleted = await Product.findByIdAndDelete(id);
-  }
-
-  if (!deleted) {
-    deleted = await Product.findOneAndDelete({ _id: id });
-  }
-
-  return new Response(JSON.stringify({ message: "Product deleted" }), {
     headers: { "Content-Type": "application/json" },
   });
 }
