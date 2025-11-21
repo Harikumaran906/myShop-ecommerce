72e2dbb7008f7d3ad6c880af1630b366eab959d0
Harikumaran906 <kumaranh906@gmail.com>
2025-11-21 12:34:33 +0530

    Switch fetch to /api and update Stripe URLs

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -1,24 +1,62 @@
-export const dynamic = "force-dynamic";
-
 import { connectDB } from "../../../../lib/db";
 import { Product } from "../../../../lib/productModel";
+import mongoose from "mongoose";
 
-export async function GET(_, { params }) {
+export async function GET(request, { params }) {
   await connectDB();
-  const product = await Product.findById(params.id);
-  return Response.json(product);
+
+  const id = params.id;
+
+  let product;
+
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    product = await Product.findById(id);
+  }
+
+  if (!product) {
+    product = await Product.findOne({ _id: id });
+  }
+
+  return new Response(JSON.stringify(product), {
+    headers: { "Content-Type": "application/json" },
+  });
 }
 
 export async function PUT(request, { params }) {
   await connectDB();
+  const id = params.id;
   const body = await request.json();
-  const updated = await Product.findByIdAndUpdate(params.id, body, { new: true });
 
-  return Response.json(updated);
+  let updated;
+
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    updated = await Product.findByIdAndUpdate(id, body, { new: true });
+  }
+
+  if (!updated) {
+    updated = await Product.findOneAndUpdate({ _id: id }, body, { new: true });
+  }
+
+  return new Response(JSON.stringify(updated), {
+    headers: { "Content-Type": "application/json" },
+  });
 }
 
-export async function DELETE(_, { params }) {
+export async function DELETE(request, { params }) {
   await connectDB();
-  await Product.findByIdAndDelete(params.id);
-  return Response.json({ message: "Product deleted" });
+  const id = params.id;
+
+  let deleted;
+
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    deleted = await Product.findByIdAndDelete(id);
+  }
+
+  if (!deleted) {
+    deleted = await Product.findOneAndDelete({ _id: id });
+  }
+
+  return new Response(JSON.stringify({ message: "Product deleted" }), {
+    headers: { "Content-Type": "application/json" },
+  });
 }
